cbc21f470b65eec969e4716ee27335ae1f0e32e5
DilanMartinez16 <dilanjmartinezm16@gmail.com>
2025-05-26 20:24:26 -0500

    Update help_desk_JCBD.sql
    Actualización de BBDD

--- help_desk_JCBD.sql
+++ help_desk_JCBD.sql
@@ -1,10 +1,9 @@
--- Active: 1744258976597@@127.0.0.1@3306@help_desk_jcbd
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-04-2025 a las 20:52:00
+-- Tiempo de generación: 26-05-2025 a las 19:25:56
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -13,15 +12,11 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-USE Help_desk_JCBD;
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
---
 --
 
 -- --------------------------------------------------------
@@ -78,6 +73,7 @@ CREATE TABLE `tickets` (
   `id_ticket` int(11) NOT NULL,
   `prioridad` varchar(100) NOT NULL,
   `estado_ticket` varchar(100) NOT NULL DEFAULT 'nuevo',
+  `tipo` varchar(100) NOT NULL,
   `titulo` varchar(200) NOT NULL,
   `descripcion` text NOT NULL,
   `grupo` varchar(100) NOT NULL,
@@ -96,12 +92,11 @@ CREATE TABLE `tickets` (
 
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
-  `nombres` varchar(100) NOT NULL,
-  `apellidos` varchar(100) NOT NULL,
+  `nombre_completo` varchar(100) NOT NULL,
   `correo` varchar(100) NOT NULL,
   `telefono` varchar(20) NOT NULL,
   `nombre_usuario` varchar(50) NOT NULL,
-  `contraseña` varchar(255) NOT NULL,
+  `contraseña` varchar(100) NOT NULL,
   `rol` varchar(100) NOT NULL,
   `estado` varchar(100) NOT NULL DEFAULT 'activo',
   `id_entidad1` int(11) DEFAULT NULL,
@@ -112,11 +107,11 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `nombres`, `apellidos`, `correo`, `telefono`, `nombre_usuario`, `contraseña`, `rol`, `estado`, `id_entidad1`, `fecha_registro`) VALUES
-(1, 'Admin', 'Sistema', 'admin@helpdeskjcbd.com', '123456789', 'admin', 'admin123', 'administrador', 'activo', 1, '2025-04-09 01:39:51'),
-(2, 'Prueba', 'Sistema', 'prueba1@helpdeskjcbd.com', '123456789', 'prueba_1', '12345', 'usuario', 'activo', 2, '2025-04-09 01:39:51'),
-(3, 'Sistema', 'Sistema', 'prueba2@helpdeskjcbd.com', '123456789', 'prueba_2', '1234', 'usuario', 'activo', 3, '2025-04-09 01:39:51'),
-(4, 'Ad', 'tecnico', 'tecnico1@helpdeskjcbd.com', '123456789', 'tecnico_1', '123456', 'tecnico', 'activo', 1, '2025-04-09 01:39:51');
+INSERT INTO `usuarios` (`id_usuario`, `nombre_completo`, `correo`, `telefono`, `nombre_usuario`, `contraseña`, `rol`, `estado`, `id_entidad1`, `fecha_registro`) VALUES
+(1, 'Admin Sistema', 'admin@helpdeskjcbd.com', '123456789', 'admin', 'admin123', 'administrador', 'activo', 1, '2025-04-09 06:39:51'),
+(2, 'Prueba Usuario', 'prueba1@helpdeskjcbd.com', '123456789', 'prueba_1', '12345', 'usuario', 'activo', 2, '2025-04-09 06:39:51'),
+(3, 'Sistema Usuario', 'prueba2@helpdeskjcbd.com', '123456789', 'prueba_2', '1234', 'usuario', 'activo', 3, '2025-04-09 06:39:51'),
+(4, 'Técnico Principal', 'tecnico1@helpdeskjcbd.com', '123456789', 'tecnico_1', '123456', 'tecnico', 'activo', 1, '2025-04-09 06:39:51');
 
 -- --------------------------------------------------------
 
